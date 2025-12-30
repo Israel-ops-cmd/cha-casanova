bf6572cb0968e14980e4677f17100065fff9dff2
Israel-ops-cmd <israellipe2020@gmail.com>
2025-12-30 09:18:39 -0300

    creating utils

--- src/components/hero/style.ts
+++ src/components/hero/style.ts
@@ -1,19 +1,20 @@
 import styled from 'styled-components'
+import { pxToRem } from '../../utils/utils'
 
 export const Container = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    margin: 20px 40px 0px 40px;
+    margin: ${pxToRem(20)} ${pxToRem(40)} 0 ${pxToRem(40)};
 `
 export const NamesContainer = styled.p`
-    font-size: 30px;
+    font-size: ${pxToRem(30)};
 `
 export const Elements = styled.nav`
     ul {
         display: flex;
         flex-direction: row;
-        gap: 20px;
+        gap: ${pxToRem(30)};
         list-style: none;
         margin: 0;
         padding: 0;
